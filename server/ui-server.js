b3f1c25fc27e4460ca962ccab0d1db9156d05499
Young Hahn <young@developmentseed.org>
2011-03-01 11:38:38 -0500

    Implement Backbone REST endpoints generically.

--- server/ui-server.js
+++ server/ui-server.js
@@ -7,6 +7,24 @@ module.exports = function(server, settings) {
     // Set up the backbone router
     new Router();
 
+    // Route middleware for validating a model.
+    function validateModel(req, res, next) {
+        if (models[req.param('model')]) {
+            next();
+        } else {
+            next(new Error('Invalid model.'));
+        }
+    };
+
+    // Route middleware for validating a collection.
+    function validateCollection(req, res, next) {
+        if (models[req.param('model') + 'List']) {
+            next();
+        } else {
+            next(new Error('Invalid collection.'));
+        }
+    };
+
     // Send settings to the browser.
     server.get('/settings.js', function(req, res, next) {
         res.send(
@@ -15,21 +33,49 @@ module.exports = function(server, settings) {
         );
     });
 
-    // GET endpoint for TilesetList collection.
-    server.get('/api/tileset', function(req, res, next) {
-        var tilesets = new models.TilesetList();
-        tilesets.fetch({
+    // Generic GET endpoint for collection loading.
+    server.get('/api/:model', validateCollection, function(req, res, next) {
+        var list = new models[req.param('model') + 'List']({ id: req.params[0] });
+        list.fetch({
             success: function(model, resp) { res.send(model.toJSON()) },
             error: function(model, resp) { res.send(resp, 500); }
         });
     });
 
-    // GET endpoint for Tileset model.
-    server.get('/api/tileset/*', function(req, res, next) {
-        var tileset = new models.Tileset({ id: req.params[0] });
-        tileset.fetch({
+    // Generic GET endpoint for model loading.
+    server.get('/api/:model/*', validateModel, function(req, res, next) {
+        var model = new models[req.param('model')]({ id: req.params[0] });
+        model.fetch({
             success: function(model, resp) { res.send(model.toJSON()) },
             error: function(model, resp) { res.send(resp, 500); }
         });
     });
+
+    // Generic POST endpoint for model creation.
+    server.post('/api/:model/*', validateModel, function(req, res, next) {
+        var model = new models[req.param('model')]({ id: req.params[0] });
+        model.save(req.body, {
+            success: function(model, resp) { res.send(resp) },
+            error: function(model, resp) { res.send(resp, 500); }
+        });
+    });
+
+    // Generic PUT endpoint for model update.
+    server.put('/api/:model/*', validateModel, function(req, res, next) {
+        var model = new models[req.param('model')]({ id: req.params[0] });
+        model.save(req.body, {
+            success: function(model, resp) { res.send(resp) },
+            error: function(model, resp) { res.send(resp, 500); }
+        });
+    });
+
+    // Generic DELETE endpoint for model deletion.
+    server.del('/api/:model/*', validateModel, function(req, res, next) {
+        var model = new models[req.param('model')]({ id: req.params[0] });
+        model.destroy({
+            success: function(model, resp) { res.send({}) },
+            error: function(model, resp) { res.send(resp, 500); }
+        });
+    });
 }
+
