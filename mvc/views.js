b0b86679ed4611775c1bed8f4729523f28b46e91
Young Hahn <young@developmentseed.org>
2011-03-02 02:30:05 -0500

    Consolidate HUD functionality in base view.

--- mvc/views.js
+++ mvc/views.js
@@ -113,15 +113,10 @@ var OpenLayersView = Backbone.View.extend({
     }
 });
 
-// TilesetView
+// HUDView
 // -------
-// View for exploring a single Tileset. Provides a fullscreen OpenLayers UI with
-// HUD panels for enabled features (e.g. info, download, etc.)
-var TilesetView = Backbone.View.extend({
-    initialize: function(options) {
-        _.bindAll(this, 'render', 'ready', 'controlZoom', 'format');
-        this.render().trigger('attach');
-    },
+// Base view that supports toggling on/off HUD displays.
+var HUDView = Backbone.View.extend({
     events: {
         'click .buttons a': 'hud'
     },
@@ -144,6 +139,17 @@ var TilesetView = Backbone.View.extend({
             });
         }
         return false;
+    }
+});
+
+// TilesetView
+// -----------
+// View for exploring a single Tileset. Provides a fullscreen OpenLayers UI with
+// HUD panels for enabled features (e.g. info, download, etc.)
+var TilesetView = HUDView.extend({
+    initialize: function(options) {
+        _.bindAll(this, 'render', 'ready', 'controlZoom', 'format');
+        this.render().trigger('attach');
     },
     format: function(type, value) {
         switch (type) {
@@ -217,10 +223,9 @@ var TilesetView = Backbone.View.extend({
 });
 
 // TilesetListView
-// -----------
+// ---------------
 // View showing each tileset as a thumbnail. Main tileset browsing page.
-var TilesetListView = Backbone.View.extend({
-    templateName: 'TilesetListView',
+var TilesetListView = HUDView.extend({
     initialize: function(options) {
         _.bindAll(this, 'render');
         this.render().trigger('attach');
@@ -237,7 +242,7 @@ var TilesetListView = Backbone.View.extend({
 });
 
 // TilesetRowView
-// ----------
+// --------------
 // View for a single tileset in a TilesetListView.
 var TilesetRowView = Backbone.View.extend({
     tagName: 'li',
