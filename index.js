b8ef279e0c0bd1872f4e8ddaef6542a35ef9abb8
Konstantin Käfer <github@kkaefer.com>
2011-07-01 19:05:33 +0200

    add tilesource protocols manually

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 #!/usr/bin/env node
 process.title = 'tilestream';
+
+var tilelive = require('tilelive');
+tilelive.protocols['mbtiles:'] = require('mbtiles');
+
 require('bones').load(__dirname);
 !module.parent && require('bones').start();
 
