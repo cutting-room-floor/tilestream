efecfddb5dd493d4ec325a75b8a901553d7e670c
Dane Springmeyer <dane@dbsgeo.com>
2013-11-14 17:15:10 -0800

    another windows fix mapping HOME->USERPROFILE

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 #!/usr/bin/env node
+
+// increase the libuv threadpool size to 1.5x the number of logical CPUs.
+process.env.UV_THREADPOOL_SIZE = Math.ceil(Math.max(4, require('os').cpus().length * 1.5));
+
 process.title = 'tilestream';
+
+if (process.platform === 'win32') {
+    // HOME is undefined on windows
+    process.env.HOME = process.env.USERPROFILE;
+}
 
 var tilelive = require('tilelive');
 require('mbtiles').registerProtocols(tilelive);
